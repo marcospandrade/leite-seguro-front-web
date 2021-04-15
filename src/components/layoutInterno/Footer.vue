6702a4f0472e815af06e677258cb1d54f06d3a9a
Marcos Andrade <marcos.andrade@pitang.com>
2021-04-15 00:27:59 -0300

    header + footer interno

---
+++ src/components/layoutInterno/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+    <div class="container text-center mt-3">
+      <span class="text-muted ">Leite Seguro 2021</span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterInterno",
+};
+</script>
+
+<style>
+.footer {
+  position: fixed;
+  left: 0px;
+  bottom: 0px;
+  height: 10vh;
+  width: 100%;
+  background: #50e3c2;
+}
+</style>
--- src/modules/dashboard/pages/Dashboard.vue
+++ src/modules/dashboard/pages/Dashboard.vue
@@ -1,19 +1,29 @@
 <template>
-    <div class="container">
-        Dashboard works!
-    </div>
+  <div>
+    <header-interno></header-interno>
+    <div class="container">Dashboard works!</div>
+    <footer-interno></footer-interno>
+  </div>
 </template>
 
 <script>
+import HeaderInterno from "@/components/layoutInterno/Header";
+import FooterInterno from "@/components/layoutInterno/Footer";
+
 export default {
-    name: "dashboard",
-    data() {
-        return {
+  name: "dashboard",
+  components: {
+    HeaderInterno,
+    FooterInterno,
+  },
 
-        }
-    },
-    methods: {
-
-    }
-}
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
+
+<style>
+
+</style>
