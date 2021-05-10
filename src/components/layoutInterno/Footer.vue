59f56c4c997007a05e6e1765402ef1957e9526be
Marcos Andrade <marcos.andrade@pitang.com>
2021-05-10 08:38:54 -0300

    data studio

--- src/components/layoutInterno/Footer.vue
+++ src/components/layoutInterno/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer">
+  <footer class="footer mt-auto">
     <div class="container text-center mt-3">
       <h5 class="footer-leite-seguro">Leite Seguro 2021</h5>
     </div>
@@ -14,17 +14,17 @@ export default {
 
 <style>
 .footer {
-  position: fixed;
-  left: 0px;
-  bottom: 0px;
-  height: 10vh;
+  position: sticky;
+  bottom: 0;
   width: 100%;
+  height: 60px;
+  line-height: 60px;
   background-image: linear-gradient(-45deg, #152649 0%, #4f525c 100%);
 }
 
 .footer-leite-seguro {
+  padding-top: 3vh;
   font-weight: bolder;
-  color:white;
+  color: white;
 }
-
 </style>
--- src/modules/relatorios/pages/Relatorios.vue
+++ src/modules/relatorios/pages/Relatorios.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <header-interno></header-interno>
-    <div class="container">Relatorio works</div>
+
+    <div class="container" id="div-relatorio">
+      <h3> Acompanhe os relatórios aqui: </h3>
+      <iframe
+        width="1100"
+        height="650"
+        src="https://datastudio.google.com/embed/reporting/e4fa2758-049e-442f-8d07-d89c16a55dd0/page/7MzHC"
+        frameborder="0"
+        style="border: 0"
+        allowfullscreen
+      ></iframe>
+    </div>
+
     <footer-interno></footer-interno>
   </div>
 </template>
@@ -13,8 +25,18 @@ import FooterInterno from "@/components/layoutInterno/Footer";
 export default {
   name: "Relatorios",
   components: {
-      HeaderInterno,
-      FooterInterno
-  }
+    HeaderInterno,
+    FooterInterno,
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+  #div-relatorio {
+    margin-bottom: 10vh;
+  }
+
+  #div-relatorio h3 {
+    margin: 2vh 0vw 2vh 0vw;
+  }
+</style>
